6f878d6ebe77b23e11a74a107cafdcefc61f1736
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-12-07 11:22:53 +0200

    init project

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from logging import basicConfig
+from database import db, setup_database
+from utils.main import get_env
+
+basicConfig(filename='basic.log')
+load_dotenv(override=True)
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = get_env('DB_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFACTIONS']  = False
+    app.config['JSON_AS_ASCII'] = False
+
+    db.init_app(app)
+    Migrate(app, db, render_as_batch=True)
+  
+    return app
+
+app = create_app()
+setup_database(app)
+
+if __name__ == '__main__':
+    app.run(debug=bool(get_env('DEBUG')))
+
---
+++ models/model.py
@@ -0,0 +1,88 @@
+from database import db
+import enum
+from sqlalchemy import Column, Integer, String, Enum, Date, DECIMAL, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Role(enum.Enum):
+    customer = 1
+    executor = 2
+
+class User(db.Model): #type: ignore
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    age = Column(Integer)
+    email = Column(String(100))
+    role = Column(Enum(Role))
+    phone = Column(String)
+
+    orders_customer = relationship('Order', foreign_keys='Order.customer_id')
+    orders_executor = relationship("Order", foreign_keys="Order.executor_id")
+    offers = relationship("Offer", back_populates='user')
+
+    @property
+    def serialized(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role.name,
+            "phone": self.phone,
+        }
+
+    def __repr__(self) -> str:
+        return f'<User id:{self.id} name:{self.first_name} {self.last_name} />'
+
+class Offer(db.Model): #type: ignore
+    __tablename__ = 'offers'
+
+    id = Column(Integer, primary_key=True)
+
+    executor_id = Column(Integer, ForeignKey('users.id'))
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    
+    user = relationship('User',back_populates='offers')
+    order = relationship('Order', back_populates='offers')
+
+    def __repr__(self):
+        return f'<Offer id:{self.id} user_id:{self.executor_id} />'
+
+
+class Order(db.Model): #type: ignore
+    __tablename__ = 'orders'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    description = Column(String(250))
+    start_date = Column(Date)
+    end_date = Column(Date)
+    address  = Column(String(100))
+    price = Column(DECIMAL)
+
+    customer_id = Column(Integer, ForeignKey('users.id'))
+    executor_id = Column(Integer, ForeignKey('users.id'))
+
+    user_customer = relationship(User, foreign_keys=customer_id,  overlaps="orders_customer")
+    user_executor = relationship(User, foreign_keys=executor_id,  overlaps="orders_executor")
+
+    offers = relationship(Offer, back_populates='order')
+
+    @property
+    def serialized(self):
+        return {
+            "id": self.id,
+            "first_name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price
+        }
+
+
+    def __repr__(self):
+        return f'<Order id:{self.id} name:{self.name} address:{self.address} />'
