2551ba050196b46564bbaf5ec0880dace9d9be87
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-12-09 18:40:11 +0200

    add logs for user crud

--- app/users/api.py
+++ app/users/api.py
@@ -9,19 +9,31 @@ user_dao = UserDao()
 
 @users_blueprint.route('/')
 def get_users():
-    users = user_dao.get_users()
+    try:
+        users = user_dao.get_users()
 
-    return jsonify(users)
+        return jsonify(users)
+    except Exception as e:
+        logging.exception(f'get users unknown error')
+        return jsonify({'message': str(e), "status": -1}), 500
 
 
 @users_blueprint.route('/<int:id>/')
 def get_user(id: int):
-    user = user_dao.get_user(id)
+    try:
+        user = user_dao.get_user(id)
 
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
+        if not user:
+            logging.error(f'User with id {id} not found')
+            return jsonify({'message': 'User not found', "status": 1}), 404
 
-    return jsonify(user)
+        logging.info(f'User with id {id} found')
+
+        return jsonify(user)
+    except Exception as e:
+        logging.exception('get_user unknown error')
+        return jsonify({'message': str(e), "status": -1}), 500
+
 
 @users_blueprint.route('/', methods=['POST'])
 def create_user():
@@ -35,10 +47,14 @@ def create_user():
         return jsonify(user.serialized)
     except TypeError as e:
         logging.exception('create_user failed')
-        return jsonify({'message': str(e)}), 400
+        return jsonify({'message': str(e), "status": 1}), 400
     except ValueError as e:
         logging.exception('create_user failed')
-        return jsonify({'message': str(e)}), 400
+        return jsonify({'message': str(e), "status": 2}), 400
+    except Exception as e:
+        logging.exception(f'create_user unknown')
+        return jsonify({'message': str(e), "status": -1}), 500
+
 
 @users_blueprint.route('/<int:id>/', methods=['DELETE'])
 def delete_user(id: int):
@@ -50,11 +66,15 @@ def delete_user(id: int):
     except TypeError as e:
         logging.exception(f'delete user failed id:{id}')
 
-        return jsonify({'message': str(e)}), 400
+        return jsonify({'message': str(e), "status": 1}), 400
     except NotFoundError as e:
         logging.exception(f'delete user failed not found id: {id}')
 
-        return jsonify({'message': str(e)}), 404
+        return jsonify({'message': str(e), "status": 2}), 404
+    except Exception as e:
+        logging.exception('delete user unknown error id: {id}')
+        return jsonify({'message': str(e), "status": -1}), 500
+
 
 @users_blueprint.route('/<int:id>/', methods=['PUT'])
 def update_user(id: int):
@@ -64,16 +84,19 @@ def update_user(id: int):
         user = user_dao.update_user(id, data)
         logging.info(f'succesfully update user id:{id} user: {user}')
 
-        return jsonify({'message': 'success'})
+        return jsonify(user)
     except ValueError as e:
         logging.exception(f'update user failed id:{id}')
 
-        return jsonify({'message': str(e)}), 400
+        return jsonify({'message': str(e), "status": 1}), 400
     except TypeError as e:
         logging.exception(f'update user failed id:{id}')
 
-        return jsonify({'message': str(e)}), 400
+        return jsonify({'message': str(e), "status": 2}), 400
     except NotFoundError as e:
         logging.exception(f'delete user failed not found id: {id}')
 
-        return jsonify({'message': str(e)}), 404
+        return jsonify({'message': str(e), "status": 3}), 404
+    except Exception as e:
+        logging.exception(f'delete user uknown error')
+        return jsonify({'message': str(e), "status": -1}), 500
