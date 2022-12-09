80ec344379b502e639242d7b0c7df3f029774483
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-12-09 18:29:59 +0200

    add offer crud operations

--- app/offers/api.py
+++ app/offers/api.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .dao.offer_dao import OfferDao
+from classes.exception import ValidationDataError, NotFoundError
+import logging
 
 offers_blueprint = Blueprint('offers', __name__, url_prefix='/api/v1/offers')
 offer_dao = OfferDao()
@@ -7,16 +9,77 @@ offer_dao = OfferDao()
 
 @offers_blueprint.route('/')
 def get_offers():
-    users = offer_dao.get_offers()
+    offers = offer_dao.get_offers()
 
-    return jsonify(users)
+    return jsonify(offers)
 
 
 @offers_blueprint.route('/<int:id>/')
 def get_offer(id: int):
-    user = offer_dao.get_offer(id)
+    offer = offer_dao.get_offer(id)
 
-    if not user:
+    if not offer:
         return jsonify({'message': 'Offer not found'}), 404
 
-    return jsonify(user)
+    return jsonify(offer)
+
+
+@offers_blueprint.route('/', methods=['POST'])
+def create_offer():
+    data = request.json()
+
+    try:
+        offer = offer_dao.create_offer(data)
+
+        return jsonify(offer)
+    except TypeError as e:
+        logging.exception(f'create offer error data: {data}')
+        return jsonify({'message': str(e), "status": 1})
+    except ValidationDataError as e:
+        logging.exception(f'validation data error: {data}')
+        return jsonify({'message': str(e), "status": 2})
+    except NotFoundError as e:
+        logging.exception(f'offer not found error data: {data}')
+        return jsonify({'message': str(e), "status": 3})
+    except Exception as e:
+        logging.exception(f'offer create unknown error data: {data}')
+        return jsonify({'message': str(e), "status": 4})
+
+
+@offers_blueprint.route('/<int:id>/', methods=['PUT'])
+def update_offer(id: int):
+    data = request.json()
+
+    try:
+        offer = offer_dao.update_offer(id, offer)
+
+        return jsonify(offer)
+    except TypeError as e:
+        logging.exception(f'update offer error id: {id}, data error: {data}')
+        return jsonify({'message': str(e), "status": 1})
+    except ValidationDataError as e:
+        logging.exception(f'update offer error id: {id}, data: {data}')
+        return jsonify({'message': str(e), "status": 2})
+    except NotFoundError as e:
+        logging.exception(f'update offer error id: {id}, data: {data}')
+        return jsonify({'message': str(e), "status": 3})
+    except Exception as e:
+        logging.exception(f'update offer unknown error id: {id}, data: {data}')
+        return jsonify({'message': str(e), "status": 4})
+
+
+@offers_blueprint.route('/<int: id>/', methods=['DELETE'])
+def delete_offer(id: int):
+    try:
+        offer_dao.delete_offer(id)
+
+        return jsonify({"message": "success"})
+    except TypeError as e:
+        logging.exception(f'delete offer error id: {id}')
+        return jsonify({'message': str(e), "status": 1})
+    except NotFoundError as e:
+        logging.exception(f'delete offer error id: {id}')
+        return jsonify({'message': str(e), "status": 2})
+    except Exception as e:
+        logging.exception(f'delete offer error id: {id}')
+        return jsonify({'message': str(e), "status": 3})
--- app/offers/dao/offer_dao.py
+++ app/offers/dao/offer_dao.py
@@ -1,5 +1,6 @@
-from models.model import Offer
+from models.model import Offer, User, Order
 from classes.dao import Dao
+from classes.exception import NotFoundError,  ValidationDataError
 
 
 class OfferDao(Dao):
@@ -13,6 +14,10 @@ class OfferDao(Dao):
 
         return offers
 
+    @property
+    def create_offer_keys(self):
+        return {"executor_id", 'order_id'}
+
     def get_offer(self, id: int) -> dict | None:
         self.set_query()
 
@@ -22,3 +27,73 @@ class OfferDao(Dao):
             return None
 
         return offer.serialized
+
+    def create_offer(self, data: dict) -> dict:
+        if type(data) is not dict:
+            raise TypeError(f'Uncorrect type of data')
+
+        data_keys = set(data.keys())
+
+        if data_keys != self.create_offer_keys:
+            raise ValidationDataError(
+                f'Uncorrect data for create, need to define {self.offer_create_keys.difference(data_keys)}')
+
+        if User.query.get(data['executor_id']) is None:
+            raise NotFoundError(
+                f'Executor id:{data["executor_id"]} does not exist')
+
+        if Order.query.get(data['order_id']) is None:
+            raise NotFoundError(f'Order id:{data["order_id"]} does not exist')
+
+        offer = Offer(
+            executor_id=data['executor_id'], order_id=data['order_id'])
+
+        self.add_to_db(data)
+
+        return offer.serialized
+
+    def update_order(self, id: int, data: dict) -> dict:
+        if type(id) is not int:
+            raise TypeError(f'Uncorrect type of id, must be integer')
+
+        if type(data) is not dict:
+            raise TypeError(f'Uncorrect type of data, must be dictionary')
+
+        data_keys = set(data.keys())
+
+        if data_keys != self.create_offer_keys:
+            raise ValidationDataError(
+                f'Uncorrect data for update, need to define {self.create_offer_key.difference(data_keys)}')
+
+        if User.query.get(data['executor_id']) is None:
+            raise NotFoundError(
+                f'Executor id:{data["executor_id"]} does not exist')
+
+        if Order.query.get(data['order_id']) is None:
+            raise NotFoundError(f'Order id:{data["order_id"]} does not exist')
+
+        self.set_query()
+
+        offer = self.__query.get(id)
+
+        for k, v in data.items():
+            setattr(offer, k, v)
+
+        self.update_data_in_db(offer)
+
+        return offer.serialized
+
+    def delete_offer(self, id: int) -> True:
+        if type(id) is not int:
+            raise TypeError('id must be an integer')
+
+        self.set_query()
+
+        offer = self.__query.get(id)
+
+        if not offer:
+            raise NotFoundError(f'Offer not found id: {id}')
+
+        self.delete_from_db(offer)
+
+        return True
